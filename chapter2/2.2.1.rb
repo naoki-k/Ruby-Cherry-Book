eb046cba3c0d851fb9f888574d83376e1d2bf3d2
n.kakuta <naoki.k.0820.i@gmail.com>
2018-12-10 18:12:56 +0900

    2.2.1 puts result

--- chapter2/2.2.1.rb
+++ chapter2/2.2.1.rb
@@ -3,13 +3,20 @@
 
 # 文字列
 '1'.to_s
+puts('1'.to_s)
 # 数値
 1.to_s
+puts(1.to_s)
 # nil
 nil.to_s
+puts(nil.to_s)
+puts("_"+nil.to_s+"_")
 # true
 true.to_s
+puts(true.to_s)
 # false
 false.to_s
+puts(false.to_s)
 # 正規表現
 /\d+/.to_s
+puts(/\d+/.to_s)
