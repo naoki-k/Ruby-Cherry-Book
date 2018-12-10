49ea9b69d4c91c0f216040cb3df75dd67f251f64
n.kakuta <naoki.k.0820.i@gmail.com>
2018-12-11 01:19:06 +0900

    2.2.7 puts result

--- chapter2/2.2.7.rb
+++ chapter2/2.2.7.rb
@@ -6,15 +6,16 @@
 
 # 数値
 123.to_s
-
+puts(123)
 # 文字列
 'Hello'.to_s
-
+puts('Hello')
 # 配列
 [1, 2, 3].to_s
-
+puts([1, 2, 3])
 # ハッシュ
 {'japan' => 'yen', 'us' => 'dollar', 'india' => 'rupee'}.to_s
-
+puts({'japan' => 'yen', 'us' => 'dollar', 'india' => 'rupee'})
 # 正規表現
 /\d+-\d+/.to_s
+puts(/\d+-\d+/)
