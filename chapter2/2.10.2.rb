396cf7684630aaaa2cce9309a968226963a2ea74
n.kakuta <naoki.k.0820.i@gmail.com>
2019-02-03 10:39:30 +0900

    2.10.2 create

---
+++ chapter2/2.10.2.rb
@@ -0,0 +1,27 @@
+# not and or
+
+# 優先順位
+# => 高い   !
+#          &&
+#          ||
+#          not
+#   低い   and or
+
+t1 = true
+f1 = false
+
+puts "!f1 || t1" # => true
+puts ">> #{!f1 || t1}"
+
+puts "not f1 || t1" # => false
+puts ">> #{not f1 || t1}"
+
+
+# and
+# 制御フローに用いる
+user.valid? and send_mail_to user
+
+# or
+# 真でなければ〜する，と使う
+# countryがnilまたはfalseなら早期return
+country or return "countryを入力してください"
