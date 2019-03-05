4a982b7a7ea1d76106b895d92b4d33ca23a054ea
n.kakuta <naoki.k.0820.i@gmail.com>
2019-03-05 20:54:41 +0900

    to_ints method create

--- ruby-book/lib/rgb.rb
+++ ruby-book/lib/rgb.rb
@@ -3,3 +3,11 @@ def to_hex(r, g, b)
     hex += num.to_s(16).rjust(2, '0')
   end
 end
+
+def to_ints(hex)
+  r,g,b= hex[1..2],hex[3..4],hex[5..6]
+  ints = []
+  [r,g,b].map do |str|
+    str.hex
+  end
+end
