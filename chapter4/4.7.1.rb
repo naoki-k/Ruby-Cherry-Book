4b93a286c4c3dd3dfd0e21602193ccf45d3987e8
n.kakuta <naoki.k.0820.i@gmail.com>
2019-03-07 16:09:01 +0900

    4.7.2 create

--- chapter4/4.7.1.rb
+++ chapter4/4.7.1.rb
@@ -1,16 +1,16 @@
 # 配列の取得方法
-a = [1,2,3,4,5]
-puts a[1,3] # => [1,3,5]
+a = [1, 2, 3, 4, 5]
+puts a[1, 3] # => [1,3,5]
 puts('__')
-puts a.values_at(0,2,4) # => [1,3,5]
+puts a.values_at(0, 2, 4) # => [1,3,5]
 puts('__')
 
-a = [1,2,3]
+a = [1, 2, 3]
 puts a[-1]
 puts('__')
 puts a[-2]
 puts('__')
-puts a[-2,2]
+puts a[-2, 2]
 puts('__')
 puts a.last
 puts('__')
