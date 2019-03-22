c5a6b66163c7e0c0de5ded0aafdbaaeddbf82d86
n.kakuta <naoki.k.0820.i@gmail.com>
2019-03-22 11:53:50 +0900

    gate system update

--- ruby-book/lib/gate.rb
+++ ruby-book/lib/gate.rb
@@ -1,2 +1,26 @@
 class Gate
+  STATIONS = %i[umeda juso mikuni].freeze
+  FARES = [150, 190].freeze
+
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def enter(ticket)
+    ticket.stamp(@name)
+  end
+
+  def exit(ticket)
+    fare = calc_fare(ticket)
+    ticket.fare >= fare
+  end
+
+  private def calc_fare(ticket)
+    from = STATIONS.index(ticket.stamped_at)
+    to = STATIONS.index(@name)
+    distance = (from - to).abs
+    FARES[distance - 1]
+  end
 end
